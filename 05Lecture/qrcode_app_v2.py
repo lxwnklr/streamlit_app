a3d484aba241982ca9f8eeda1949c7cc20d4c344
lxwnklr <winkleralexa@yahoo.com>
2024-11-19 12:01:58 +0100

    Update qrcode_app_v2.py

--- 05Lecture/qrcode_app_v2.py
+++ 05Lecture/qrcode_app_v2.py
@@ -5,7 +5,7 @@ from decode_qrcode_page import decode_qrcode_page
 from qrcode_generator_app import qrcode_generator_page
 
 st.set_page_config(page_title= "Hi", page_icon="laughing")
-st.image("images/IMG_1901.jpg")
+st.image("IMG_1901.jpg")
 
 options =["Create QR code", "Decode QR code", "About me"]
 page_selection =st.sidebar.selectbox("Menu", options)
