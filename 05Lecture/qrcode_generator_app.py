cfc06b4333dfff7fd5fc1c3523260960699813a8
lxwnklr <winkleralexa@yahoo.com>
2024-11-19 12:02:22 +0100

    Update qrcode_generator_app.py

--- 05Lecture/qrcode_generator_app.py
+++ 05Lecture/qrcode_generator_app.py
@@ -3,7 +3,7 @@ import segno
 import time
 
 st.set_page_config(page_title= "Moin", page_icon="laughing")
-st.image("images/IMG_1901.jpg")
+st.image("IMG_1901.jpg")
 
 def qrcode_generator_page():
     st.title("QR Code Generator")
@@ -18,13 +18,13 @@ def qrcode_generator_page():
 
     def generate_qrcode(qr_url, dark_colour):
         qrcode=segno.make_qr(qr_url)
-        qrcode.save("images/qrcode_saved.png", scale=5)
+        qrcode.save("qrcode_saved.png", scale=5)
 
     if button and qr_url:
         with st.spinner("Generate QR Coe:"):
             time.sleep(2)
         generate_qrcode(qr_url, dark_colour)
-        st.image("images/qrcode_saved.png", caption= qr_url)
+        st.image("qrcode_saved.png", caption= qr_url)
 
     # warning for when user clicks on button without a url
     if button and qr_url == "":
